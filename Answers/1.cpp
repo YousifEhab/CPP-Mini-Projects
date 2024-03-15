0ebe4c5c562344add400fb5b6b7eed5318d26a52
Yousif Ehab <145108594+YousifEhab@users.noreply.github.com>
2024-03-15 06:01:12 +0200

    1.cpp

---
+++ Answers/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "enter Integer number ";
+    int Number;
+    cin >> Number;
+    cout << "The negative value of " << Number << " is " << Number * -1;
+    // system("pause");
+    return 0;
+}
