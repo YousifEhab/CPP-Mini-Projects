bc38774528a95aabb04914c11e80c9df7b0c3d5f
Yousif Ehab <145108594+YousifEhab@users.noreply.github.com>
2024-05-13 23:56:45 +0300

    Create Group-C.cpp

---
+++ 3-Practical/Final-Practical/Group-C.cpp
@@ -0,0 +1,30 @@
+// Lab Exam - Group C
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size, adult = 0, kid = 0;
+    int arr[10];
+    cout << "Enter a series of 10 shoes sizes:\n";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Enter shoe number " << i + 1 << " size --> ";
+        cin >> size;
+        if (size >= 13)
+        {
+            adult++;
+        }
+        else
+        {
+            kid++;
+        }
+    }
+    if (adult > kid)
+    {
+        cout << "We need to order " << adult - kid << " kids shoes";
+    }
+    else
+    {
+        cout << "We are good to go";
+    }
+}
